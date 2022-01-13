80702d7219e8bcc8e929786903dab231cb181c59
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2022-01-13 22:16:28 +0100

    updates to createBooking

--- src/components/CreateBooking.js
+++ src/components/CreateBooking.js
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import apiUtils from "../utils/apiUtils"
 
 const CreateBooking = () => {
   const [booking, setBooking] = useState({});
+  const [assistants, setAssistants] = useState([]);
+  const [selectedAssistants, setSelectedAssistants] = useState([]);
   const [statusMessage, setStatusMessage] = useState("");
   const [msgColor, setMsgColor] = useState("");
 
@@ -10,6 +12,25 @@ const CreateBooking = () => {
 
   const handleInput = (event) => {
     setBooking({ ...booking, [event.target.id]: event.target.value })
+  }
+
+  useEffect(() => {
+    const getAssistants = async () => {
+      const response = await apiUtils.getAuthAxios().get(URL + '/assistant/all')
+      setAssistants(response.data.assistants);
+    }
+    getAssistants()
+  }, [URL]);
+
+  const addAssistant = (assistantId, checked) => {
+    if (checked) {
+      selectedAssistants.push({ id: assistantId })
+      setSelectedAssistants(selectedAssistants)
+    }
+
+    if (!checked) {
+      setSelectedAssistants(selectedAssistants.filter(a => a.id !== assistantId))
+    }
   }
 
   const createBooking = async () => {
@@ -22,11 +43,7 @@ const CreateBooking = () => {
           make: booking.make,
           year: booking.year
         },
-        assistants: [
-          {
-            id: booking.id
-          }
-        ],
+        assistants: selectedAssistants,
         user: localStorage.getItem('user')
       })
       setMsgColor('#4caf50');
@@ -47,8 +64,14 @@ const CreateBooking = () => {
         <input className="form-control addInput" id="model" placeholder="Enter car model" type="text"></input>
         <input className="form-control addInput" id="make" placeholder="Enter car make" type="text"></input>
         <input className="form-control addInput" id="year" placeholder="Enter car year" type="text"></input>
-        <input className="form-control addInput" id="id" placeholder="Enter ID of desired assistant" type="text"></input>
       </form>
+      <h4>Choose assistants</h4>
+      <ul>
+        {assistants.map((a) =>
+          <li key={a.id}>
+            {a.name} <input onChange={(event) => addAssistant(event.target.value, event.target.checked)} type="checkbox" id="assistantId" name={a.name} value={a.id}></input>
+          </li>)}
+      </ul>
       <button onClick={createBooking} className="btn btn-primary mt-3">Create</button>
     </div>
   )
